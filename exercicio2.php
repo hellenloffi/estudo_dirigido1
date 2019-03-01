42931b795d21d75418123d4d5e5643a80ac24940
Hellen Loffi <48135684+hellenloffi@users.noreply.github.com>
2019-03-01 16:04:28 -0300

    Add files via upload

---
+++ exercicio2.php
@@ -0,0 +1,17 @@
+<?php 
+    
+    print "digite uma nota";
+    $nota1 = fgets(STDIN)
+    
+    print "digite uma nota";
+    $nota2 = fgets(STDIN)
+    
+    print "digite uma nota";
+    $nota3 = fgets(STDIN)
+
+    print "digite uma nota";
+    $nota4 = fgets(STDIN)
+
+$media = ($nota1 + $nota2 + $nota3 + $nota3 + $nota4 )
+    print "A média é $media";
+ 
